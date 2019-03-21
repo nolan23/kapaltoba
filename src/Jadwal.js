6bc2e4321ae021c46ec82d84163edc10165f7a35
nolan23 <robyparapat23@gmail.com>
2019-03-21 20:49:20 +0700

    remove npm install and npm build in .travis.yml

---
+++ src/Jadwal.js
@@ -0,0 +1,19 @@
+import React, {Component, Fragment} from 'react';
+import { withStyles } from '@material-ui/core';
+
+class JadwalContainer extends Component{
+    constructor(){
+        super(props);
+
+    }
+
+    render(){
+        return(
+            <Fragment>
+                
+            </Fragment>
+        );
+    }
+}
+
+export default withStyles(styles)(JadwalContainer);
